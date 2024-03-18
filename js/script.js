81f78c7246df3bced4008c33443ff7fb2afbee46
Sanjay Singh <30376256+SanjaySRocks@users.noreply.github.com>
2024-03-19 00:19:48 +0530

    add certificate api

--- js/script.js
+++ js/script.js
@@ -16,7 +16,24 @@ form.addEventListener('submit', function (e) {
             timer: 2000
         });
 
-    fetch('http://nileshvishwa.com:3000/')
+    let btn = document.getElementById("submitbtn");
+    const originalText = btn.innerText;
+
+    btn.innerText = "Processing...";
+
+    fetch('https://cg.nileshvishwa.com/generate', 
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                fullName: fullName.value,
+                phoneNo: phoneNo.value,
+                email: email.value
+            })
+
+        })
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
@@ -24,13 +41,28 @@ form.addEventListener('submit', function (e) {
             return response.json();
         })
         .then(data => {
+            
             console.log(data);
-            swal(data);
+            swal({
+                title: 'Certificate Generated !',
+                text: `Id: ${data.CertificateDetails._id } (${fullName.value}) !`, 
+                icon: "success"
+            });
+
+            var downloadTab = window.open(`https://cg.nileshvishwa.com/download/${data.CertificateDetails._id}.pdf`, '_blank');
+            setTimeout(function() {
+                downloadTab.close();
+              }, 5000);
+
+            btn.innerText = originalText;
         })
         .catch(error => {
             console.error('Error:', error);
             swal(error, "error");
+
+            btn.innerText = originalText;
+
         });
 
-
+        
 })
